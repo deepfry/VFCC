345c11acef4d60e6f14255dd7fc897d118a3d0b4
deepfry <jj@4cc.co>
2018-03-25 15:06:47 -0700

    testing again

--- assets/js/zz_custom.js
+++ assets/js/zz_custom.js
@@ -107,7 +107,7 @@ $('form.form-email.custom-script').submit(function(e){
 	formSuccess = body.find('.form-success');
 	thisForm.addClass('attempted-submit');
 	if (mr.forms.validateFields($('form.form-email.custom-script')) !== 1 && grecaptcha.getResponse() != '' && /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/igm.test($('textarea').val()) != true){
-		return
+		$('form.form-email.custom-script').submit()
 		// $.post(thisForm.attr("action"), thisForm.serialize()).then(function() {
 		// 	window.location = thisForm.attr("action")
 		// });
